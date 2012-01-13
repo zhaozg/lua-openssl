2eafbab1f359c8b562a55090bff7b5162eb492e1
George  Zhao <zhaozg@gmail.com>
2012-01-13 09:04:37 +0800

    update for license

--- src/pkcs7.c
+++ src/pkcs7.c
@@ -1,10 +1,24 @@
+/*
+   +----------------------------------------------------------------------+
+   | PHP Version 5                                                        |
+   +----------------------------------------------------------------------+
+   | Copyright (c) 1997-2012 The PHP Group                                |
+   +----------------------------------------------------------------------+
+   | This source file is subject to version 3.01 of the PHP license,      |
+   | that is bundled with this package in the file LICENSE, and is        |
+   | available through the world-wide-web at the following url:           |
+   | http://www.php.net/license/3_01.txt                                  |
+   | If you did not receive a copy of the PHP license and are unable to   |
+   | obtain it through the world-wide-web, please send a note to          |
+   | license@php.net so we can mail you a copy immediately.               |
+   +----------------------------------------------------------------------+
+*/
 /*=========================================================================*\
 * PKCS7 routines
 * lua-openssl toolkit
 *
-* $Id$
-* $Source$
-* $Header$
+* This product includes PHP software, freely available from <http://www.php.net/software/>
+* Author:  george zhao <zhaozg(at)gmail.com>
 \*=========================================================================*/
 #include "openssl.h"
 
