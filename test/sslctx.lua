00470cc78d5aa25360734f2e28aec268564b95ea
zhaozg <zhaozg@gmail.com>
2014-11-01 12:10:29 +0800

    add missing file

---
+++ test/sslctx.lua
@@ -0,0 +1,82 @@
+local openssl = require'openssl'
+local ssl,pkey,x509 = openssl.ssl,openssl.pkey,openssl.x509
+
+local M = {}
+
+local function load(path)
+    local f = assert(io.open(path,'r'))
+    if f then
+        local c = f:read('*all')
+        f:close()
+        return c
+    end
+end
+
+function M.new(params)
+--[[
+local params = {
+   mode = "server",
+   protocol = "tlsv1",
+   key = "../certs/serverAkey.pem",
+   certificate = "../certs/serverA.pem",
+   cafile = "../certs/rootA.pem",
+   verify = {"peer", "fail_if_no_peer_cert"},
+   options = {"all", "no_sslv2"},
+   password = 'password'
+}
+--]]    
+    local protocol = params.protocol and string.upper(string.sub(params.protocol,1,3))
+        ..string.sub(params.protocol,4,-1) or 'TLSv1_2'
+    local ctx = ssl.ctx_new(protocol,params.ciphers)
+    local xkey = nil
+    if (type(params.password)=='nil') then
+        xkey = assert(pkey.read(load(params.key),true,'pem'))
+    elseif (type(params.password)=='string')  then
+        xkey = assert(pkey.read(load(params.key),true,'pem',params.password))
+    elseif (type(params.password)=='function') then
+        local p = assert(params.password())
+        xkey = assert(pkey.read(load(params.key),true,'pem',p))
+    end
+
+    assert(xkey)
+    local xcert = nil
+    if (params.certificate) then
+        xcert = assert(x509.read(load(params.certificate)))
+    end
+    assert(ctx:use( xkey, xcert))
+
+    if(params.cafile or params.capath) then
+        ctx:verify_locations(params.cafile,params.capath)
+    end
+
+    unpack = unpack or table.unpack   
+    if(params.verify) then
+        local args = {}
+        for i=1,#params.verify do
+            table.insert(args, params.verify[i])
+        end
+        ctx:set_verify(args)
+    end
+    if params.options then
+        local args = {}
+        for i=1,#params.options do
+            table.insert(arg,params.options[i])
+        end
+        ctx:options(unpack(args))
+    end
+    if params.verifyext then
+        for k,v in pairs(params.verifyext) do
+            params.verifyext[k] = string.gsub(v,'lsec_','')        
+        end
+        ctx:set_cert_verify(params.verifyext)
+    end
+    if params.dhparam then
+        ctx:set_tmp('dh',params.dhparam)
+    end
+    if params.curve then
+        ctx:set_tmp('ecdh',params.curve)
+    end
+    return ctx
+end
+
+return M
