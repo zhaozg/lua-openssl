a3081ab7a221666b87cb914ba39f3119c00bea4c
zhaozg <zhaozg@gmail.com>
2015-02-28 21:16:32 +0800

    update test

--- test/0.tcp.lua
+++ test/0.tcp.lua
@@ -1,63 +1,113 @@
-local ok, uv = pcall(require, 'lluv')
+local ok, uv  = pcall(require,'luv')
 if not ok then uv = nil end
 
-local lua_spawn do
 local LUA = arg[-1]
-
-local function P(pipe, read)
-  return {
-    stream = pipe,
-    flags = uv.CREATE_PIPE + 
-            (read and uv.READABLE_PIPE or uv.WRITABLE_PIPE)
-  }
-end
-
-lua_spawn = function(f, o, e, c)
-    return uv.spawn({
-        file = LUA, args = {f},
-      stdio = {{}, P(o, false), P(e, false)}
-    }, c)
-end
-end
-
 TestTCP = {}
+
 if uv then
-function TestEngine:testTCP()
-    local function onread(pipe, err, chunk)
-        if err then
-            if err:name() ~= 'EOF' then
-                assert(not err, tostring(err))
+    function TestTCP:testUVTcp() 
+        local lcode = 1
+        local stdout1 = uv.new_pipe(false)
+        local stderr1 = uv.new_pipe(false)
+        local stdout2 = uv.new_pipe(false)
+        local stderr2 = uv.new_pipe(false)
+        
+        local function onread(err, chunk)
+          assert(not err, err)
+          if (chunk) then
+            print(chunk)
+          else
+            print("end")
+          end
+        end
+
+        local child, pid child, pid = uv.spawn(LUA, {
+          args = {"0.tcp_s.lua"},
+          stdio = {nil, stdout1, stderr1}
+        }, function (code, signal)
+            assertEquals(code,0)
+            uv.close(child)
+            lcode = 0
+        end)
+        os.execute('ping -n 3 127.0.0.1')
+        local child, pid child, pid = uv.spawn(LUA, {
+          args = {"0.tcp_c.lua"},
+          stdio = {nil, stdout2, stderr2}
+        }, function (code, signal)
+            assertEquals(code,0)
+            uv.close(child)
+            lcode = 0
+        end)
+        
+        uv.read_start(stdout1, onread)
+        uv.read_start(stderr1, onread)
+        uv.read_start(stdout2, onread)
+        uv.read_start(stderr2, onread)
+        
+        uv.run()
+        uv.loop_close()
+        assertEquals(lcode,0)
+    end
+end
+
+
+local ok, luv = pcall(require, 'llluv')
+if not ok then luv = nil end
+
+if luv then
+
+    local function P(pipe, read)
+      return {
+        stream = pipe,
+        flags = luv.CREATE_PIPE + 
+                (read and luv.READABLE_PIPE or luv.WRITABLE_PIPE)
+      }
+    end
+
+    lua_spawn = function(f, o, e, c)
+        return luv.spawn({
+            file = LUA, args = {f},
+          stdio = {{}, P(o, false), P(e, false)}
+        }, c)
+    end
+    end
+
+    if luv then
+    function TestTCP:testTCP()
+        local function onread(pipe, err, chunk)
+            if err then
+                if err:name() ~= 'EOF' then
+                    assert(not err, tostring(err))
+                end
+            end
+            if chunk then
+                print(chunk)
+            else
+                print("end")
             end
         end
-        if chunk then
-            print(chunk)
-        else
-            print("end")
+
+        local function onclose(child, err, status)
+            if err then return print("Error spawn:", err) end
+            assertEquals(status, 0)
+            child:close()
         end
+
+        local stdout1 = luv.pipe()
+        local stderr1 = luv.pipe()
+        local stdout2 = luv.pipe()
+        local stderr2 = luv.pipe()
+
+        lua_spawn("0.tcp_s.lua", stdout1, stderr1, onclose)
+        os.execute('ping -n 3 127.0.0.1')
+        lua_spawn("0.tcp_c.lua", stdout2, stderr2, onclose)
+
+        stdout1:start_read(onread)
+        stderr1:start_read(onread)
+        stdout2:start_read(onread)
+        stderr2:start_read(onread)
+
+        luv.run()
+        luv.close()
     end
-
-    local function onclose(child, err, status)
-        if err then return print("Error spawn:", err) end
-        assertEquals(status, 0)
-        child:close()
-    end
-
-    local stdout1 = uv.pipe()
-    local stderr1 = uv.pipe()
-    local stdout2 = uv.pipe()
-    local stderr2 = uv.pipe()
-
-    lua_spawn("0.tcp_s.lua", stdout1, stderr1, onclose)
-    os.execute('ping -n 3 127.0.0.1')
-    lua_spawn("0.tcp_c.lua", stdout2, stderr2, onclose)
-
-    stdout1:start_read(onread)
-    stderr1:start_read(onread)
-    stdout2:start_read(onread)
-    stderr2:start_read(onread)
-
-    uv.run()
-    uv.close()
 end
-end
-
--- test/8.ssl.lua
+++ test/8.ssl.lua
@@ -1,121 +1,297 @@
-local ok, uv = pcall(require, 'lluv')
+local ok, uv  = pcall(require,'luv')
 if not ok then uv = nil end
 
-local lua_spawn do
+TestSSL = {}
 local LUA = arg[-1]
 
-local function P(pipe, read)
-  return {
-    stream = pipe,
-    flags = uv.CREATE_PIPE + 
-            (read and uv.READABLE_PIPE or uv.WRITABLE_PIPE)
-  }
-end
-
-lua_spawn = function(f, o, e, c)
-    return uv.spawn({
-        file = LUA, args = {f},
-      stdio = {{}, P(o, false), P(e, false)}
-    }, c)
-end
-end
-
-local function onread(pipe, err, chunk)
-    if err then
-        if err:name() ~= 'EOF' then
-            assert(not err, tostring(err))
-        end
-        pipe:close()
-    end
-
-    if chunk then
-        print(chunk)
-    else
-        print("end")
-    end
-end
-
-local function onclose(child, err, status)
-    if err then return print("Error spawn:", err) end
-    assertEquals(status, 0)
-    child:close()
-end
-
-TestSSL = {}
 if uv then
-function TestSSL:testSSL()
-    local stdout1 = uv.pipe()
-    local stderr1 = uv.pipe()
-    local stdout2 = uv.pipe()
-    local stderr2 = uv.pipe()
-    
-    lua_spawn("8.ssl_s.lua", stdout1, stderr1, onclose)
-    os.execute('ping -n 3 127.0.0.1')
-    lua_spawn("8.ssl_c.lua", stdout2, stderr2, onclose)
+    function TestSSL:testSSL()
+        local lcode
+        local stdout1 = uv.new_pipe(false)
+        local stderr1 = uv.new_pipe(false)
+        local stdout2 = uv.new_pipe(false)
+        local stderr2 = uv.new_pipe(false)
+        local function onread(err, chunk)
+          assert(not err, err)
+          if (chunk) then
+            print(chunk)
+          else
+            print("end")
+          end
+        end
 
-    stdout1:start_read(onread)
-    stderr1:start_read(onread)
-    stdout2:start_read(onread)
-    stderr2:start_read(onread)
+        local child, pid child, pid = uv.spawn(arg[-1], {
+          args = {"8.ssl_s.lua"},
+          stdio = {nil, stdout1, stderr1}
+        }, function (code, signal)
+            assertEquals(code,0)
+            uv.close(child)
+            lcode = code
+        end)
 
-    uv.run()
-    uv.close()
+        local child, pid child, pid = uv.spawn(arg[-1], {
+          args = {"8.ssl_c.lua"},
+          stdio = {nil, stdout2, stderr2}
+        }, function (code, signal)
+            assertEquals(code,0)
+            uv.close(child)
+            lcode = code
+        end)
+        uv.read_start(stdout1, onread)
+        uv.read_start(stderr1, onread)
+        uv.read_start(stdout2, onread)
+        uv.read_start(stderr2, onread)
+        
+        uv.run()
+        uv.loop_close()
+        assertEquals(lcode,0)
+    end
+
+
+    function TestSSL:testBio()
+        local lcode 
+        local stdout1 = uv.new_pipe(false)
+        local stderr1 = uv.new_pipe(false)
+        local stdout2 = uv.new_pipe(false)
+        local stderr2 = uv.new_pipe(false)
+        local function onread(err, chunk)
+          assert(not err, err)
+          if (chunk) then
+            print(chunk)
+          else
+            print("end")
+          end
+        end
+        local child, pid child, pid = uv.spawn(arg[-1], {
+          args = {"8.bio_s.lua"},
+          stdio = {nil, stdout1, stderr1}
+        }, function (code, signal)
+            assertEquals(code,0)
+            uv.close(child)
+            lcode = code
+        end)
+
+        local child, pid child, pid = uv.spawn(arg[-1], {
+          args = {"8.bio_c.lua"},
+          stdio = {nil, stdout2, stderr2}
+        }, function (code, signal)
+            assertEquals(code,0)
+            uv.close(child)
+            lcode = 0
+        end)
+        uv.read_start(stdout1, onread)
+        uv.read_start(stderr1, onread)
+        uv.read_start(stdout2, onread)
+        uv.read_start(stderr2, onread)
+        
+        uv.run()
+        uv.loop_close()
+        assertEquals(lcode,0)
+    end
+
+
+    function TestSSL:testsslconnectbio()
+        local lcode
+        local stdout1 = uv.new_pipe(false)
+        local stderr1 = uv.new_pipe(false)
+        local stdout2 = uv.new_pipe(false)
+        local stderr2 = uv.new_pipe(false)
+        local function onread(err, chunk)
+          assert(not err, err)
+          if (chunk) then
+            print(chunk)
+          else
+            print("end")
+          end
+        end
+        local child, pid child, pid = uv.spawn(arg[-1], {
+          args = {"8.bio_s.lua"},
+          stdio = {nil, stdout1, stderr1}
+        }, function (code, signal)
+            assertEquals(code,0)
+            uv.close(child)
+            lcode = code
+        end)
+
+        local child, pid child, pid = uv.spawn(arg[-1], {
+          args = {"8.ssl_c.lua"},
+          stdio = {nil, stdout2, stderr2}
+        }, function (code, signal)
+            assertEquals(code,0)
+            uv.close(child)
+            lcode = code
+        end)
+        uv.read_start(stdout1, onread)
+        uv.read_start(stderr1, onread)
+        uv.read_start(stdout2, onread)
+        uv.read_start(stderr2, onread)
+        
+        uv.run()
+        uv.loop_close()
+        assertEquals(lcode,0)
+    end
+
+    function TestSSL:testbioconnectssl()
+        local lcode = nil
+        local stdout1 = uv.new_pipe(false)
+        local stderr1 = uv.new_pipe(false)
+        local stdout2 = uv.new_pipe(false)
+        local stderr2 = uv.new_pipe(false)
+        local function onread(err, chunk)
+          assert(not err, err)
+          if (chunk) then
+            print(chunk)
+          else
+            print("end")
+          end
+        end
+        local child, pid child, pid = uv.spawn(arg[-1], {
+          args = {"8.ssl_s.lua"},
+          stdio = {nil, stdout1, stderr1}
+        }, function (code, signal)
+            assertEquals(code,0)
+            uv.close(child)
+            lcode = code
+        end)
+
+        local child, pid child, pid = uv.spawn(arg[-1], {
+          args = {"8.bio_c.lua"},
+          stdio = {nil, stdout2, stderr2}
+        }, function (code, signal)
+            assertEquals(code,0)
+            uv.close(child)
+            lcode = code
+        end)
+        uv.read_start(stdout1, onread)
+        uv.read_start(stderr1, onread)
+        uv.read_start(stdout2, onread)
+        uv.read_start(stderr2, onread)
+        
+        uv.run()
+        uv.loop_close()
+        assertEquals(lcode,0)
+    end
 end
 
-function TestSSL:testBio()
-    local stdout1 = uv.pipe()
-    local stderr1 = uv.pipe()
-    local stdout2 = uv.pipe()
-    local stderr2 = uv.pipe()
-    
-    lua_spawn("8.bio_s.lua", stdout1, stderr1, onclose)
-    os.execute('ping -n 3 127.0.0.1')
-    lua_spawn("8.bio_c.lua", stdout2, stderr2, onclose)
 
-    stdout1:start_read(onread)
-    stderr1:start_read(onread)
-    stdout2:start_read(onread)
-    stderr2:start_read(onread)
+local ok, luv = pcall(require, 'llluv')
+if not ok then luv = nil end
 
-    uv.run()
-    uv.close()
-end
 
-function TestSSL:testsslconnectbio()
-    local stdout1 = uv.pipe()
-    local stderr1 = uv.pipe()
-    local stdout2 = uv.pipe()
-    local stderr2 = uv.pipe()
-    
-    lua_spawn("8.bio_s.lua", stdout1, stderr1, onclose)
-    os.execute('ping -n 3 127.0.0.1')
-    lua_spawn("8.ssl_c.lua", stdout2, stderr2, onclose)
+local lua_spawn do
 
-    stdout1:start_read(onread)
-    stderr1:start_read(onread)
-    stdout2:start_read(onread)
-    stderr2:start_read(onread)
+    local function P(pipe, read)
+      return {
+        stream = pipe,
+        flags = luv.CREATE_PIPE + 
+                (read and luv.READABLE_PIPE or luv.WRITABLE_PIPE)
+      }
+    end
 
-    uv.run()
-    uv.close()
-end
+    lua_spawn = function(f, o, e, c)
+        return luv.spawn({
+            file = LUA, args = {f},
+          stdio = {{}, P(o, false), P(e, false)}
+        }, c)
+    end
+    end
 
-function TestSSL:testbioconnectssl()
-    local stdout1 = uv.pipe()
-    local stderr1 = uv.pipe()
-    local stdout2 = uv.pipe()
-    local stderr2 = uv.pipe()
-    
-    lua_spawn("8.ssl_s.lua", stdout1, stderr1, onclose)
-    os.execute('ping -n 3 127.0.0.1')
-    lua_spawn("8.bio_c.lua", stdout2, stderr2, onclose)
+    local function onread(pipe, err, chunk)
+        if err then
+            if err:name() ~= 'EOF' then
+                assert(not err, tostring(err))
+            end
+            pipe:close()
+        end
 
-    stdout1:start_read(onread)
-    stderr1:start_read(onread)
-    stdout2:start_read(onread)
-    stderr2:start_read(onread)
+        if chunk then
+            print(chunk)
+        else
+            print("end")
+        end
+    end
 
-    uv.run()
-    uv.close()
-end
+    local function onclose(child, err, status)
+        if err then return print("Error spawn:", err) end
+        assertEquals(status, 0)
+        child:close()
+    end
+
+
+    if luv then
+    function TestSSL:testSSL()
+        local stdout1 = luv.pipe()
+        local stderr1 = luv.pipe()
+        local stdout2 = luv.pipe()
+        local stderr2 = luv.pipe()
+        
+        lua_spawn("8.ssl_s.lua", stdout1, stderr1, onclose)
+        os.execute('ping -n 3 127.0.0.1')
+        lua_spawn("8.ssl_c.lua", stdout2, stderr2, onclose)
+
+        stdout1:start_read(onread)
+        stderr1:start_read(onread)
+        stdout2:start_read(onread)
+        stderr2:start_read(onread)
+
+        luv.run()
+        luv.close()
+    end
+
+    function TestSSL:testBio()
+        local stdout1 = luv.pipe()
+        local stderr1 = luv.pipe()
+        local stdout2 = luv.pipe()
+        local stderr2 = luv.pipe()
+        
+        lua_spawn("8.bio_s.lua", stdout1, stderr1, onclose)
+        os.execute('ping -n 3 127.0.0.1')
+        lua_spawn("8.bio_c.lua", stdout2, stderr2, onclose)
+
+        stdout1:start_read(onread)
+        stderr1:start_read(onread)
+        stdout2:start_read(onread)
+        stderr2:start_read(onread)
+
+        luv.run()
+        luv.close()
+    end
+
+    function TestSSL:testsslconnectbio()
+        local stdout1 = luv.pipe()
+        local stderr1 = luv.pipe()
+        local stdout2 = luv.pipe()
+        local stderr2 = luv.pipe()
+        
+        lua_spawn("8.bio_s.lua", stdout1, stderr1, onclose)
+        os.execute('ping -n 3 127.0.0.1')
+        lua_spawn("8.ssl_c.lua", stdout2, stderr2, onclose)
+
+        stdout1:start_read(onread)
+        stderr1:start_read(onread)
+        stdout2:start_read(onread)
+        stderr2:start_read(onread)
+
+        luv.run()
+        luv.close()
+    end
+
+    function TestSSL:testbioconnectssl()
+        local stdout1 = luv.pipe()
+        local stderr1 = luv.pipe()
+        local stdout2 = luv.pipe()
+        local stderr2 = luv.pipe()
+        
+        lua_spawn("8.ssl_s.lua", stdout1, stderr1, onclose)
+        os.execute('ping -n 3 127.0.0.1')
+        lua_spawn("8.bio_c.lua", stdout2, stderr2, onclose)
+
+        stdout1:start_read(onread)
+        stderr1:start_read(onread)
+        stdout2:start_read(onread)
+        stderr2:start_read(onread)
+
+        luv.run()
+        luv.close()
+    end
 end
