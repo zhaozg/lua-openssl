2f5f0d029f898099aebd77e40c98f563ac892c01
zhaozg <zhaozg@gmail.com>
2015-03-08 15:48:17 +0800

    update travis test

--- .travis/setup_uv.sh
+++ .travis/setup_uv.sh
@@ -3,21 +3,17 @@
 source .travis/platform.sh
 
 cd $TRAVIS_BUILD_DIR
+git clone https://github.com/luvit/luv
+cd luv
+git submodule update --init --recursive
+git submodule update --recursive
 
-git clone https://github.com/libuv/libuv.git -b v1.x
+sudo add-apt-repository --yes ppa:kalakris/cmake
+sudo apt-get update -qq
+sudo apt-get install cmake
 
-cd libuv
-
-mkdir -p lib
-mkdir -p build
-git clone http://git.chromium.org/external/gyp.git build/gyp
-
-if [ "$PLATFORM" == "macosx" ]; then
-  ./gyp_uv.py -f xcode && xcodebuild -ARCHS="x86_64" -project uv.xcodeproj -configuration Release -target All
-  cp ./build/Release/libuv.a ./lib;
-else
-  ./gyp_uv.py -f make && BUILDTYPE=Release CFLAGS=-fPIC make -C out
-  cp ./out/Release/libuv.a ./lib;
-fi
+make
+export LUA_LIBDIR=/usr/local/lib/lua/`shell lua -e "_,_,v=string.find(_VERSION,'Lua (.+)');print(v)"`
+cp build/luv.so $(LUA_LIBDIR)
 
 cd $TRAVIS_BUILD_DIR
