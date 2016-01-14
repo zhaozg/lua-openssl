7f329fee25d8a76cd6d0a4fa29905828e7bf8156
George Zhao <zhaozg@gmail.com>
2016-01-14 15:43:09 +0800

    fix bug to generate ec_key

--- src/pkey.c
+++ src/pkey.c
@@ -204,18 +204,16 @@ static int EC_KEY_generate_key_part(EC_KEY *eckey)
     pub_key = EC_POINT_new(group);
     if (pub_key == NULL)
       goto err;
+	EC_KEY_set_public_key(eckey, pub_key);
+	EC_POINT_free(pub_key);
+	pub_key = (EC_POINT *)EC_KEY_get0_public_key(eckey);
   }
 
   if (!EC_POINT_mul(group, pub_key, priv_key, NULL, NULL, ctx))
     goto err;
-  {
-    EC_POINT_make_affine(EC_KEY_get0_group(eckey),
-                         (EC_POINT *)EC_KEY_get0_public_key(eckey),
-                         NULL);
-  }
-  EC_KEY_set_private_key(eckey, priv_key);
-  EC_KEY_set_public_key(eckey, pub_key);
-
+  EC_POINT_make_affine(EC_KEY_get0_group(eckey),
+	  pub_key,
+	  NULL);
   ok = 1;
 
 err:
@@ -464,18 +462,16 @@ static LUA_FUNCTION(openssl_pkey_new)
 
             EC_KEY_set_public_key(ec, pnt);
             EC_POINT_free(pnt);
-          }
+          }else
+			  EC_KEY_generate_key_part(ec);
 
-          if (!EVP_PKEY_assign_EC_KEY(pkey, ec))
+          if (EC_KEY_check_key(ec)==0 || EVP_PKEY_assign_EC_KEY(pkey, ec)==0)
           {
             EC_KEY_free(ec);
             EVP_PKEY_free(pkey);
             pkey = NULL;
           }
-          if (d && !EC_KEY_check_key(ec))
-          {
-            EC_KEY_generate_key_part(ec);
-          }
+
           BN_free(d);
           BN_free(x);
           BN_free(y);
