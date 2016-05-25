f4430dfe7599284c277026f40ff2b408dd257e3c
Mark Goodwin <mgoodwin@mozilla.com>
2016-05-25 14:04:55 +0100

    remove unneeded arg to check_email and check_ip_asc

--- src/x509.c
+++ src/x509.c
@@ -405,7 +405,7 @@ static LUA_FUNCTION(openssl_x509_check_email)
   if (lua_isstring(L, 2))
   {
     const char *email = lua_tostring(L, 2);
-    lua_pushboolean(L, X509_check_email(cert, email, strlen(email), 0, NULL));
+    lua_pushboolean(L, X509_check_email(cert, email, strlen(email), 0));
   } else {
     lua_pushboolean(L, 0);
   }
@@ -418,7 +418,7 @@ static LUA_FUNCTION(openssl_x509_check_ip_asc)
   if (lua_isstring(L, 2))
   {
     const char *ip_asc = lua_tostring(L, 2);
-    lua_pushboolean(L, X509_check_ip_asc(cert, ip_asc, strlen(ip_asc), 0, NULL));
+    lua_pushboolean(L, X509_check_ip_asc(cert, ip_asc, strlen(ip_asc), 0));
   } else {
     lua_pushboolean(L, 0);
   }
