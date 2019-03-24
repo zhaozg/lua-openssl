83c3a24e6432094c80f19693c5c2afa4eac4ea5e
zhaozg <zhaozg@gmail.com>
2019-03-24 11:49:43 +0800

    auto luarock publish

--- .travis/install.sh
+++ .travis/install.sh
@@ -13,17 +13,20 @@ fi
 make install PREFIX=$HOME/.usr PKG_CONFIG="PKG_CONFIG_PATH=$PKG_CONFIG_PATH pkg-config"
 
 if [ "$TAG" == "$NOW" ]; then
-  .travis/publish_rockspec.sh $TAG
+  CNT=$(luarocks search openssl $TAG | wc -l)
+  if [ "$CNT" == "4" ]; then
+    .travis/publish_rockspec.sh $TAG
+  fi
   if [[ "$TRAVIS_OS_NAME" == "osx" && -z "$SSL" ]]; then
-    sudo $HOME/.usr/bin/luarocks install openssl OPENSSL_DIR=/usr/local/opt/openssl
+    $HOME/.usr/bin/luarocks install openssl OPENSSL_DIR=/usr/local/opt/openssl
   fi
   if [[ "$TRAVIS_OS_NAME" == "osx" && -n "$SSL" ]]; then
-    sudo $HOME/.usr/bin/luarocks install openssl OPENSSL_DIR=$HOME/.usr
+    $HOME/.usr/bin/luarocks install openssl OPENSSL_DIR=$HOME/.usr
   fi
   if [[ "$TRAVIS_OS_NAME" == "linux" && -z "$SSL" ]]; then
-    sudo -H $HOME/.usr/bin/luarocks install openssl
+    $HOME/.usr/bin/luarocks install openssl
   fi
   if [[ "$TRAVIS_OS_NAME" == "linux" && -n "$SSL" ]]; then
-    sudo -H $HOME/.usr/bin/luarocks install openssl OPENSSL_DIR=$HOME/.usr
+    $HOME/.usr/bin/luarocks install openssl OPENSSL_DIR=$HOME/.usr
   fi
 fi
