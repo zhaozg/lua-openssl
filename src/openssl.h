a6b7c53500155b8d45ccbc813e36a8bed93fc73b
zhaozg <zhaozg@gmail.com>
2019-05-19 21:01:46 +0800

    Release 0.7.5-2

--- src/openssl.h
+++ src/openssl.h
@@ -50,12 +50,13 @@
 /*History
   2017-04-18  update to 0.7.1
   2017-08-04  update to 0.7.3
-  2019-xx-xx  update to 0.7.5
+  2019-03-24  update to 0.7.5-1
+  2019-05-19  update to 0.7.5-2
 */
 
 /*                              MNNFFPPS  */
-#define LOPENSSL_VERSION_NUM  0x0070500f
-#define LOPENSSL_VERSION  "0.7.5"
+#define LOPENSSL_VERSION_NUM  0x0070502f
+#define LOPENSSL_VERSION  "0.7.5-2"
 
 #if OPENSSL_VERSION_NUMBER >= 0x10000000L
 #include <openssl/lhash.h>
