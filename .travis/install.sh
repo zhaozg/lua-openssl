dea161f0bec574123044557ccbb7ec25e855846b
zhaozg <zhaozg@gmail.com>
2020-05-10 21:10:08 +0800

    active coverage

--- .travis/install.sh
+++ .travis/install.sh
@@ -10,4 +10,10 @@ PKG_CONFIG_PATH=$HOME/.usr/lib/pkgconfig
 if [[ "$TRAVIS_OS_NAME" == "osx" && -z "$SSL" ]]; then
   PKG_CONFIG_PATH=/usr/local/opt/openssl/lib/pkgconfig:$PKG_CONFIG_PATH
 fi
+if [[ "$TRAVIS_OS_NAME" == "linux" && -z "$SSL" ]]; then
+  pip install cpp-coveralls
+  export CFLAGS="-g -fPIC -fprofile-arcs -ftest-coverage"
+  export LDFLAGS="-g -fprofile-arcs"
+fi
+
 make install PREFIX=$HOME/.usr PKG_CONFIG="PKG_CONFIG_PATH=$PKG_CONFIG_PATH pkg-config"
