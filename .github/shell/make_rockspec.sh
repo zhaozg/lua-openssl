d463ac2afccb9fb27e4b6998de54ca44588826ce
zhaozg <zhaozg@gmail.com>
2023-03-13 00:05:42 +0800

    ci: update make_rockspec.sh

--- .github/shell/make_rockspec.sh
+++ .github/shell/make_rockspec.sh
@@ -9,13 +9,13 @@ fi
 # .rockspec
 cp openssl-scm-0.rockspec openssl-${version}.rockspec
 script="/^version/s@\"[^\"]\\+\"@\"${version}\"@"
-sed -e "${script}" -i openssl-${version}.rockspec
+sed -e "${script}" -i.bak openssl-${version}.rockspec
 script="s@https://github.com/zhaozg/lua-openssl/archive/master.zip@https://github.com/zhaozg/lua-openssl/releases/download/$version/openssl-$version.tar.gz@"
-sed -e "${script}" -i openssl-${version}.rockspec
+sed -e "${script}" -i.bak openssl-${version}.rockspec
 
 # .tar.gz
 rm -rf openssl-${version}
 mkdir -p openssl-${version}/deps
-cp -r LICENSE README.md *.win lib test Makefile samples src deps openssl-${version}/
+cp -r LICENSE README.md *.win test Makefile src deps openssl-${version}/
 COPYFILE_DISABLE=true tar -czf openssl-${version}.tar.gz openssl-${version}
 rm -rf openssl-${version}
