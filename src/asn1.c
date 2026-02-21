f00603a92411c49874d4e730f85474640f9e8ec1
zhaozg <zhaozg@gmail.com>
2026-02-21 21:10:01 +0800

    asn1: conditionalize V_ASN1_APP_CHOOSE for LibreSSL
    close #405, https://github.com/openbsd/src/commit/44d602d67e24782dc4300880c01bb6cad85c8648

--- src/asn1.c
+++ src/asn1.c
@@ -22,7 +22,9 @@ static LuaL_Enumeration asn1_const[] = {
   { "CONSTRUCTED",       V_ASN1_CONSTRUCTED       },
   { "PRIMITIVE_TAG",     V_ASN1_PRIMITIVE_TAG     },
   { "PRIMATIVE_TAG",     V_ASN1_PRIMATIVE_TAG     },
+#ifdef V_ASN1_APP_CHOOSE
   { "APP_CHOOSE",        V_ASN1_APP_CHOOSE        },
+#endif
   { "OTHER",             V_ASN1_OTHER             },
   { "ANY",               V_ASN1_ANY               },
 
